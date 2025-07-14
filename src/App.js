007a12e3072fcb1b8472e4744725160b18bc46c9
Ramesh Sudhanpu <madhavi.richlabz@gmail.com>
2025-07-14 16:12:56 +0530

    Orders page chnages

--- src/App.js
+++ src/App.js
@@ -42,8 +42,7 @@ import ManageCoupons from './pages/admin/manageCoupons.jsx';
 import AddSlider from './pages/admin/sliders.jsx';
 import ManageSliders from './pages/admin/manageSliders.jsx';
 import ChangePassword from './pages/admin/changePassword.jsx';
-<<<<<<< HEAD
-import { ToastContainer, toast } from 'react-toastify';
+// import { ToastContainer, toast } from 'react-toastify';
 import OffersList from './pages/admin/OffersList.jsx';
 import CreateCoupon from './pages/admin/CreateCoupon.jsx';
 import HomePromotionCategory from './pages/admin/HomePromotionCategory.jsx';
@@ -51,10 +50,8 @@ import HomeScreenCreatePromotion from './pages/admin/HomeScreenCreatePromotion.j
 import HomeScreenPromotions from './pages/admin/HomeScreenPromotions.jsx';
 import CreatePromotion from './pages/admin/CreatePromotion'
 import Slider from 'react-slick';
-=======
 import { ToastContainer, toast ,Slide, Zoom, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
->>>>>>> 06627704c876d8be66b042c0ef132bc28dd534ea
 import ManageProducts from './pages/admin/manageProduct.jsx';
 import EditProduct from './components/editProduct.jsx';
 import PromotionCategoryList from './pages/admin/PromotionCategoryList.jsx';
@@ -83,9 +80,7 @@ function App() {
       <div className="App">
         {/* <div id="spinner" className="show">Loading...</div>
         <nav className="sticky-top">Navbar</nav> */}
-<<<<<<< HEAD
         <ToastContainer />
-=======
         <ToastContainer
         icon={false}
           position="top-right"
@@ -100,7 +95,6 @@ function App() {
           theme="dark"
           transition={Slide}
         />
->>>>>>> 06627704c876d8be66b042c0ef132bc28dd534ea
 
         <Routes>
           <Route path='/' element={<HomeBannerSection />} />
