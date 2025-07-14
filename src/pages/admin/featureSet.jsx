9b713d7046be49bfec5176d3124598e909b80844
RICH LABZ <padmavathi.richlabz@gmail.com>
2025-07-14 12:48:02 +0530

    get api for features sets

--- src/pages/admin/featureSet.jsx
+++ src/pages/admin/featureSet.jsx
@@ -4,31 +4,45 @@ import Sidebar from '../../includes/sidebar';
 import '../../css/admin/style.css';
 import { BsSearch, BsArrowClockwise } from 'react-icons/bs';
 import AddFeatureSetModal from '../../components/addFeatureSetModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchFeatureSets } from '../../redux/actions/featureSetAction';
+import PaginationComponent from '../../includes/pagination';
 
 const ManageFeatureSet = () => {
+  const dispatch = useDispatch();
+  const { featureSets = [] } = useSelector((state) => state.featureSets || {});
+  console.log('featuresets', featureSets)
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   const [selectedRows, setSelectedRows] = useState([]);
-  const[showModal,setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleToggleSidebar = (collapsed) => {
     setIsSidebarCollapsed(collapsed);
   };
 
-  const [featureSets, setFeatureSets] = useState([
-    { id: 1, name: 'Pearl Features', count: 0 },
-    { id: 2, name: 'General', count: 1 },
-    { id: 3, name: 'Diamond Features', count: 2 },
-    { id: 4, name: 'Body & Design Features', count: 2 },
-    { id: 5, name: 'Additional Features', count: 2 },
-    { id: 6, name: 'Gold Features', count: 2 },
-    { id: 7, name: 'Important Note', count: 2 },
-    { id: 8, name: 'Chain Features', count: 2 },
-    { id: 9, name: 'In The Box', count: 0 },
-  ]);
-
   useEffect(() => {
-    // Simulate fetch data from API
-  }, []);
+    dispatch(fetchFeatureSets());
+  }, [dispatch]);
+
+  const filteredFeatureSets = featureSets.filter((featureSet) => {
+    const title = featureSet.title.toLowerCase();
+    const matchesSearch = title.includes(searchTerm.toLowerCase());
+    return matchesSearch;
+  });
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 10;
+  const indexOfLastRow = currentPage * rowsPerPage;
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+  const currentRows = filteredFeatureSets.slice(indexOfFirstRow, indexOfLastRow);
+  const totalPages = Math.ceil(filteredFeatureSets.length / rowsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    if (pageNumber >= 1 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
 
   const handleSelectAll = (e) => {
     if (e.target.checked) {
@@ -43,6 +57,16 @@ const ManageFeatureSet = () => {
       prev.includes(id) ? prev.filter((rowId) => rowId !== id) : [...prev, id]
     );
   };
+
+  const groupedFeatureSets = currentRows.reduce((acc, item) => {
+    const groupName = item.featureType?.name || 'Unknown';
+    if (!acc[groupName]) {
+      acc[groupName] = [];
+    }
+    acc[groupName].push(item);
+    return acc;
+  }, {});
+
 
   return (
     <div className="sidebar-mini fixed">
@@ -72,26 +96,19 @@ const ManageFeatureSet = () => {
                 <div className="row g-3 align-items-center">
                   <div className="col-md-3">
                     <div className="input-group">
-                      <input type="text" className="form-control" placeholder="Search By" />
+                      <input type="text" className="form-control" placeholder="Search By Title" value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)} />
                     </div>
                   </div>
-                  <div className="col-md-3">
-                    <select className="form-control">
-                      <option value="">- Select Status -</option>
-                      <option value="active">Active</option>
-                      <option value="inactive">Inactive</option>
-                    </select>
-                  </div>
+
                   <div className="col-md-2 d-flex gap-2">
-                    <button className="btn btn-danger">
-                      <BsSearch style={{ fontSize: '18px' }} />
-                    </button>
-                    <button className="btn btn-success">
+
+                    <button className="btn btn-success" onClick={() => setSearchTerm('')}>
                       <BsArrowClockwise style={{ fontSize: '18px' }} />
                     </button>
                   </div>
                   <div className="col-md-4 text-end">
-                    <button className="btn btn-primary" onClick={()=>setShowModal(true) } type="button">
+                    <button className="btn btn-primary" onClick={() => setShowModal(true)} type="button">
                       + Create Feature Set
                     </button>
                   </div>
@@ -103,39 +120,46 @@ const ManageFeatureSet = () => {
             <div className="card">
               <div className="card-block">
                 <div className="row mb-3">
-                  <div className="col-lg-6 d-flex align-items-center">
-                    <h3 className="me-2">Accessories</h3>
+                  {/* <div className="col-lg-6 d-flex align-items-center">
+                    <h3 className="me-2">{featureSets?.featureType?.name}</h3>
                     <span className="badge">{featureSets.length}</span>
-                  </div>
-                  <div className="col-md-6 text-end pt">
-                    <button className="btn btn-success me-1">Active</button>
-                    <button className="btn btn-default me-1">Inactive</button>
-                    <button className="btn btn-danger me-1">Front Active</button>
-                    <button className="btn btn-warning me-1">Front Inactive</button>
-                  </div>
-                </div>
-
-                <div className="feature-row">
-                  {featureSets.map((item) => (
-                    <div key={item.id} className="feature-item d-flex justify-content-between align-items-center mb-2">
-                      <div>
-                        <input
-                          type="checkbox"
-                          checked={selectedRows.includes(item.id)}
-                          onChange={() => handleRowCheckboxChange(item.id)}
-                          className="me-2"
-                        />
-                        {item.name}
+                  </div> */}
+                  {Object.entries(groupedFeatureSets).map(([featureTypeName, items]) => (
+                    <div key={featureTypeName} className="mb-4">
+                      <div className="col-lg-6 d-flex align-items-center">
+                      <h5 className="mb-2">{featureTypeName}</h5>
                       </div>
-                      <span className="badge">{item.count}</span>
+                      <div className="feature-row">
+                        {items.map((item) => (
+                          <div
+                            key={item.id}
+                            className="feature-item d-flex justify-content-between align-items-center mb-2"
+                          >
+                            <div>
+                              <input
+                                type="checkbox"
+                                checked={selectedRows.includes(item.id)}
+                                onChange={() => handleRowCheckboxChange(item.id)}
+                                className="me-2"
+                              />
+                              {item.title}
+                            </div>
+                            <span className="badge">{item.priority}</span>
+                          </div>
+                        ))}
+                      </div>
                     </div>
                   ))}
+
                 </div>
+
+
 
                 <button className="btn btn-primary mt-3">Set Priority</button>
               </div>
             </div>
-           {showModal && <AddFeatureSetModal show={showModal} onClose={()=> setShowModal(false)}/>} 
+            <PaginationComponent currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+            {showModal && <AddFeatureSetModal show={showModal} onClose={() => setShowModal(false)} />}
           </div>
         </div>
       </div>
