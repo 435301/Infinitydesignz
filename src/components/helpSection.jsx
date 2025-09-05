bfc993c70aaec4dc7d96d22ca7accb35b1fb833c
RICH LABZ <padmavathi.richlabz@gmail.com>
2025-09-05 11:02:17 +0530

    need help section redirection onclick, responsive header icons

--- src/components/helpSection.jsx
+++ src/components/helpSection.jsx
@@ -5,15 +5,33 @@ import '../css/user/bootstrap.min.css';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNeedHelpItems } from "../redux/actions/categoryAction";
 import BASE_URL from "../config/config";
+import { useNavigate } from "react-router-dom";
 
 const HelpSection = ({ title }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { items, loading, error } = useSelector((state) => state.help);
   console.log('items',items)
 
     useEffect(() => {
     dispatch(fetchNeedHelpItems());
   }, [dispatch]);
+
+    const makeSlug = (str) =>
+  str.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-+|-+$/g, "");
+
+const handleRedirect = async (item) => {
+  let parentSlug = "category";
+  
+  if (item.parentId) {
+    const res = await fetch(`${BASE_URL}/categories/${item.parentId}`);
+    const parent = await res.json();
+    parentSlug = makeSlug(parent.title);
+  }
+
+  const childSlug = `${makeSlug(item.title)}-${item.id}`;
+  navigate(`/products/main/${parentSlug}/${childSlug}`);
+};
 
     if (loading) return <p>Loading help items...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -24,7 +42,7 @@ const HelpSection = ({ title }) => {
         <h2 className="help-title text-start">{title}</h2>
         <div className="help-grid">
          {items.map((item) => (
-            <div className="help-card" key={item.id}>
+            <div className="help-card" key={item.id}  onClick={() => handleRedirect(item, items)}>
               <div className="help-card-img-wrapper">
                 <img  src={`${BASE_URL}${item.mainImage || item.appIcon || item.webImage}`}  alt={item.title} />
                 <div className="help-card-overlay">
